11e734426fb1900725822061ea6be64ebf67aa56
devarajshetty <devaraj.p1403@gmail.com>
2018-06-29 23:49:10 +0530

    otp verified

--- otp_verifiction.php
+++ otp_verifiction.php
@@ -1,9 +1,9 @@
 <?php
 if(isset($_POST['submit'])){
-
+$no=$_POST['mobile'];
 $otp=mt_rand(100000,999999);
 // Send SMS HTTP API
-$url="http://panel.adcomsolution.in/http-api.php?username=fasttnet&password=nitin123&senderid=FSTNET&route=1&number=9585850362&message=otp:$otp";
+$url="http://panel.adcomsolution.in/http-api.php?username=fasttnet&password=nitin123&senderid=FSTNET&route=1&number=$no&message=otp:$otp";
 $curl = curl_init($url);
 curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "GET");
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
@@ -12,16 +12,10 @@ $msgid=explode(":",$result);
 $msgidis=trim($msgid[1]);
 curl_close($curl);
 
-// Delivery Report Status API
-// $status_url="http://panel.adcomsolution.in/http-dlr.php?username=fasttnet&password=nitin123&msg_id=$msgidis";
-// $curl = curl_init($status_url);
-// curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "GET");
-// curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
-// $status = curl_exec($curl);
-// curl_close($curl);
 }
 if(isset($_POST['otp_submit'])){
 	$otp_pin=$_POST['otp'];
+	$otp=$_POST['odp'];
 	if($otp_pin==$otp){
 		echo "OTP is verified";
 	}
@@ -30,8 +24,10 @@ if(isset($_POST['otp_submit'])){
 	}
 
 }
+
 ?>
 <form name="form_otp_verify" action="" method="POST">
+<input type="hidden" name="odp" value="<?php echo $otp; ?>">
 <input type="text" name="otp" placeholder="enter your otp">
 <input type="submit" name="otp_submit">
 </form>
